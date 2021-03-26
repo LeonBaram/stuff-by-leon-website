124826709ec4e0f20bcf133965850fedb5fd670c
LeonBaram <leonbaram@protonmail.com>
2021-03-26 12:37:00 -0400

    added scroll-tab functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,18 @@
 const selectors = [
+  'html',
+  'body',
   'h1',
-  'nav ul',
   'nav',
+  'nav ul',
 ];
 
 const cache = {};
 
 // populate cache from array
 selectors.forEach(sel => cache[sel] = document.querySelector(sel));
+
+// get window height from root htmlm 'clientHeight' property
+const { clientHeight } = cache['html'];
 
 cache['h1'].focus({ preventScroll: true });
 
@@ -28,4 +33,49 @@ cache['nav ul'].addEventListener('click', (event) => {
   }
 });
 
-// nav scrolling
+// scroll height at which nav is flush with top of viewport
+const NAV_SCROLL_Y = 583;
+
+// custom (re-)implementation of sticky positioning (for nav bar)
+// gets rid of the "lower bound"
+window.onscroll = () => {
+
+  const { nav } = cache;
+
+  // is the view currently below nav's default position?
+  const belowNav = window.scrollY > NAV_SCROLL_Y;
+
+  // is the nav currently sticky?
+  const sticky = nav.className === 'sticky';
+
+  if (!belowNav && sticky) {
+    nav.className = '';
+  }
+
+  if (belowNav && !sticky) {
+    nav.className = 'sticky';
+  }
+}
+
+// custom replacement of tabbing behaviour
+// if element being tabbed to is out of bounds, scroll to it instead of default jumping behaviour
+cache['body'].addEventListener('focusin', (event) => {
+  const { target } = event;
+  const { y } = target.getBoundingClientRect();
+
+  const bounds = {
+    upper: window.scrollY,
+    lower: window.scrollY + clientHeight,
+  };
+
+  console.log('bounds:', bounds);
+  console.log('y:', y);
+
+  if (y < bounds.upper || y > bounds.lower) {
+    event.preventDefault();
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    });
+  }
+});
