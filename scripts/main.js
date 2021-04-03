c0fa3868ad20bd06bf9062a53cc3e91c315dfcf5
LeonBaram <leonbaram@protonmail.com>
2021-04-02 20:01:15 -0400

    began debugging scrolling mechanics

--- scripts/main.js
+++ scripts/main.js
@@ -1,81 +1,55 @@
-const selectors = [
-  'html',
-  'body',
-  'h1',
-  'nav',
-  'nav ul',
-];
+const selectors = ['html', 'body', 'h1', 'nav', 'nav ul'];
 
 const cache = {};
 
 // populate cache from array
-selectors.forEach(sel => cache[sel] = document.querySelector(sel));
+selectors.forEach((sel) => (cache[sel] = document.querySelector(sel)));
 
-// get window height from root html 'clientHeight' property
-const { clientHeight } = cache['html'];
-
-cache['h1'].focus({ preventScroll: true });
+// handle focusing; replace focus jumps with smooth scrolling
+cache.body.addEventListener('focusin', (event) => {
+  // event.preventDefault();
+  scrollIntoViewIfNeeded(event.target);
+});
 
 // handle navigation; replace anchor jumps with smooth scrolling
 // anchors are there for semantic correctness and non-JS users
 cache['nav ul'].addEventListener('click', (event) => {
   const { target } = event;
+
   if (target.tagName === 'A') {
     event.preventDefault();
 
-    // remove leading '#'
     const id = target.hash.slice(1);
 
-    const destination = document.getElementById(id);
+    const section = document.getElementById(id);
+    scrollIntoViewIfNeeded(section);
+
     const h2 = document.querySelector(`#${id} h2`);
-    h2.focus();
-    destination.scrollIntoView({ behavior: 'smooth' });
+    h2.focus({ preventScroll: true });
   }
 });
 
-// scroll height at which nav is flush with top of viewport
-const NAV_SCROLL_Y = 583;
+/**
+ * @function isInView
+ * @param {Element} element a DOM element
+ * @returns true if the given element is fully visible on the screen
+ */
+function isInView(element) {
+  const { top, bottom } = element.getBoundingClientRect();
 
-// custom (re-)implementation of sticky positioning (for nav bar)
-// gets rid of the "lower bound"
-window.onscroll = () => {
+  const upper = window.scrollY;
+  const lower = window.scrollY + window.innerHeight;
 
-  const { nav } = cache;
-
-  // is the view currently below nav's default position?
-  const belowNav = window.scrollY > NAV_SCROLL_Y;
-
-  // is the nav currently sticky?
-  const sticky = nav.className === 'sticky';
-
-  if (!belowNav && sticky) {
-    nav.className = '';
-  }
-
-  if (belowNav && !sticky) {
-    nav.className = 'sticky';
-  }
+  return upper <= top && bottom <= lower;
 }
 
-// custom replacement of tabbing behaviour
-// if element being tabbed to is out of bounds, scroll to it instead of default jumping behaviour
-cache['body'].addEventListener('focusin', (event) => {
-  const { target } = event;
-  const { y } = target.getBoundingClientRect();
-
-  const bounds = {
-    upper: window.scrollY,
-    lower: window.scrollY + clientHeight,
-  };
-
-  console.log('bounds:', bounds);
-  console.log('y:', y);
-
-  if (y < bounds.upper || y > bounds.lower) {
-    event.preventDefault();
-    target.scrollIntoView({
-      behavior: 'smooth',
-      block: 'center',
-    });
+/**
+ * @function scrollIntoViewIfNeeded
+ * calculates whether an element is in view (see isInView), and scrolls to it if it isn't
+ * @param {Element} element a DOM element
+ */
+function scrollIntoViewIfNeeded(element) {
+  if (!isInView(element)) {
+    element.scrollIntoView({ behaviour: 'smooth' });
   }
-});
+}
---
+++ scripts/temp.js
@@ -0,0 +1,112 @@
+/* 
+scrolling/focusing behaviour:
+
+when an element is focused:
+
+  if element not in view:
+
+    scroll up or down until element is about 2/3rds of the way up the screen
+
+
+*/
+const selectors = ["html", "body", "h1", "nav", "nav ul"];
+
+const cache = {};
+
+// populate cache from array
+selectors.forEach((sel) => (cache[sel] = document.querySelector(sel)));
+
+// get window height from root html 'clientHeight' property
+const { clientHeight } = cache["html"];
+
+cache.h1.focus();
+window.setTimeout(() => cache.h1.blur(), 1000);
+
+// handle navigation; replace anchor jumps with smooth scrolling
+// anchors are there for semantic correctness and non-JS users
+cache["nav ul"].addEventListener("click", (event) => {
+  const { target } = event;
+
+  if (target.tagName === "A") {
+    event.preventDefault();
+
+    // remove leading '#'
+    const id = target.hash.slice(1);
+
+    const destination = document.getElementById(id);
+    const h2 = document.querySelector(`#${id} h2`);
+    h2.focus();
+    // destination.scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// scroll height at which nav is flush with top of viewport
+const NAV_SCROLL_Y = 583;
+
+// custom (re-)implementation of sticky positioning (for nav bar)
+// gets rid of the 'lower bound'
+window.onscroll = () => {
+  const { nav } = cache;
+
+  // is the view currently below nav's default position?
+  const belowNav = window.scrollY > NAV_SCROLL_Y;
+
+  // is the nav currently sticky?
+  const sticky = nav.className === "sticky";
+
+  if (!belowNav && sticky) {
+    nav.className = "";
+  }
+
+  if (belowNav && !sticky) {
+    nav.className = "sticky";
+  }
+};
+
+// custom replacement of tabbing behaviour
+// if element being tabbed to is out of bounds, scroll to it instead of default jumping behaviour
+cache["body"].addEventListener("focusin", (event) => {
+  const { target } = event;
+  // const { y } = target.getBoundingClientRect();
+
+  // const bounds = {
+  //   upper: window.scrollY,
+  //   lower: window.scrollY + clientHeight,
+  // };
+
+  // console.log('bounds:', bounds);
+  // console.log('y:', y);
+
+  // if (y < bounds.upper || y > bounds.lower) {
+  //   event.preventDefault();
+  //   target.scrollIntoView({
+  //     behavior: 'smooth',
+  //     block: 'center',
+  //   });
+  // }
+
+  target.scrollIntoView({ behviour: "smooth" });
+});
+
+
+const inView = (element) => {
+  const { top, bottom } = element.getBoundingClientRect();
+
+  const upper = window.scrollY;
+  const lower = window.scrollY + window.innerHeight;
+
+  return upper <= top && bottom <= lower;
+};
+
+// custom implementation of Element.scrollIntoViewIfNeeded()
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoViewIfNeeded
+
+/**
+ * @function scrollIntoViewIfNeeded
+ * scrolls to element if it isn't in view
+ * @param {Element} element an element on the page
+ */
+const scrollIntoViewIfNeeded = (element) => 
+  inView(element) || element.scrollIntoView({ behviour: "smooth" });
+
+
